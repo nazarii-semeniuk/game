2dd271d3fc2c2234fc98438f4246a3448a242509
Nazarii Semeniuk <qnazar.semenyuk@gmail.com>
2022-08-08 14:03:47 +0300

    Changed player to class, removed duplicated terrain adding

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
-import { PerspectiveCamera, Scene, WebGLRenderer, Color, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Scene, WebGLRenderer, Color } from 'three';
+import { Player } from './js/Player.js';
 import { Terrain } from './js/Terrain.js';
 
 // Debug
@@ -8,90 +8,30 @@ import Stats from 'stats.js';
 const stats = new Stats();
 document.body.appendChild(stats.domElement);
 
-
 const scene = new Scene();
 scene.background = new Color('skyblue');
 
-const TerrainCreator = new Terrain(scene);
-scene.add(TerrainCreator.createTerrain());
-
-const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    2000
-);
+const terrain = new Terrain(scene);
+terrain.createTerrain();
 
 let renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const orbitControls = new OrbitControls(camera, renderer.domElement);
-orbitControls.enableDamping = true;
-orbitControls.enablePan = true;
-orbitControls.minDistance = 5;
-orbitControls.maxDistance = 20;
-orbitControls.maxPolarAngle = Math.PI / 2 - 0.05;
-orbitControls.minPolarAngle = Math.PI / 4;
-orbitControls.update();
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial( { color: new Color('orange') } );
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+const player = new Player(scene, renderer);
 
-const keys = {
-    w: false,
-    s: false,
-    a: false,
-    d: false 
-}
-
-window.addEventListener('keydown', (e) => {
-    if ( keys[ e.key ] !== undefined ) {
-        keys[ e.key ] = true;
-    }
-})
-
-window.addEventListener('keyup', (e) => {
-    if ( keys[ e.key ] !== undefined ) {
-        keys[ e.key ] = false;
-    }
-})
-
-let speed;
-let velocity = 0.0;
 
 const animate = () => {
     stats.begin();
 
-    speed = 0.0;
-    if(keys.w) {
-        speed = 0.1;
-    } else if(keys.s) {
-        speed = -0.1;
-    }
+    player.update();
 
-    velocity += ( speed - velocity ) * 0.3;
-
-    cube.translateZ(velocity);
-
-    if(keys.a) {
-        cube.rotateY(0.05);
-    } else if(keys.d) {
-        cube.rotateY(-0.05);
-    }
-
-    camera.lookAt(cube.position);
-
-    camera.position.y = cube.position.y + 3;
-    camera.position.z = cube.position.z + -6;
-    camera.position.x = cube.position.x;
-
-    renderer.render(scene, camera);
+    renderer.render(scene, player.camera);
 
     stats.end();
 
     requestAnimationFrame(animate);
 }
+
 animate();
---
+++ src/js/Player.js
@@ -0,0 +1,88 @@
+import { PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, Color } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+export class Player {
+    constructor(scene, renderer) {
+        this.scene = scene;
+        this.renderer = renderer;
+        this.keys = {
+            w: false,
+            s: false,
+            a: false,
+            d: false
+        }
+        this.spped = 0.0;
+        this.velocity = 0.0;
+        this.player;
+        this.camera;
+
+        this.spawn();
+        this.camera();
+        this.controls();
+    }
+
+    spawn() {
+        const geometry = new BoxGeometry(1, 1, 1);
+        const material = new MeshBasicMaterial( { color: new Color('orange') } );
+        this.player = new Mesh(geometry, material);
+        this.scene.add(this.player);
+    }
+
+    camera() {
+        this.camera = new PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            2000
+        );
+
+        const orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+        orbitControls.enableDamping = true;
+        orbitControls.enablePan = true;
+        orbitControls.minDistance = 5;
+        orbitControls.maxDistance = 20;
+        orbitControls.maxPolarAngle = Math.PI / 2 - 0.05;
+        orbitControls.minPolarAngle = Math.PI / 4;
+        orbitControls.update();
+    }
+
+    controls() {
+        window.addEventListener('keydown', (e) => {
+            if (this.keys[e.key] !== undefined) {
+                this.keys[e.key] = true;
+            }
+        })
+
+        window.addEventListener('keyup', (e) => {
+            if (this.keys[e.key] !== undefined) {
+                this.keys[e.key] = false;
+            }
+        })
+    }
+
+    update() {
+        this.speed = 0.0;
+        if(this.keys.w) {
+            this.speed = 0.1;
+        } else if(this.keys.s) {
+            this.speed = -0.1;
+        }
+    
+        this.velocity += ( this.speed - this.velocity ) * 0.3;
+    
+        this.player.translateZ(this.velocity);
+    
+        if(this.keys.a) {
+            this.player.rotateY(0.05);
+        } else if(this.keys.d) {
+            this.player.rotateY(-0.05);
+        }
+    
+        this.camera.lookAt(this.player.position);
+    
+        this.camera.position.y = this.player.position.y + 3;
+        this.camera.position.z = this.player.position.z + -6;
+        this.camera.position.x = this.player.position.x;
+    }
+
+}
